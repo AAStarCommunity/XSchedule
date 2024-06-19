a64c8d0a1fe24ca63e7f0b750ad92a0fb838e9a2
dylanyang <yl976931569@163.com>
2024-06-19 12:34:09 +0800

    update API

--- project/server/api.py
+++ project/server/api.py
@@ -2,6 +2,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    return 'AAStar XScheduler'
+
+
 @app.route('/api/healthz', methods=['GET'])
 def health_check():
     return jsonify({'status': 'ok'}), 200
